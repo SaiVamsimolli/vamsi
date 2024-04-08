53e942e70b9b653c1b00c887d83882d6d591495a
SaiVamsimolli <158263619+SaiVamsimolli@users.noreply.github.com>
2024-04-08 10:58:06 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,22 @@
+import openai
+from toolkit import texttospeech as tts
+openai.api_key = "pk-MzATQIcWDTIRTnUUxCtxiJNrOaOckEPbKbfeCSPpXmtoXXsP"
+openai.api_base = "https://api.pawan.krd/pai-001-light-beta/v1"
+
+print("Hi there! I'm PAI bot. How can I assist you today?")
+user_input=input("user : ")
+while user_input.lower() != "bye":
+    response = openai.ChatCompletion.create(
+        model="pai-001-light-beta",
+        messages=[
+            {'role': 'user', 'content': user_input},
+        ],
+        stream=True,
+     allow_fallback=True
+    )
+    print("friday : ")
+    for chunk in response:
+        print(chunk.choices[0].delta.get("content", ""), end="", flush=True)
+        tts.txtToSpeech(chunk.choices[0].delta.get("content", ""))
+    user_input=input("\nuser : ")
+    
